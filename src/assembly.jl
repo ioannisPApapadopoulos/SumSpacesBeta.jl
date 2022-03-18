3e30264d7ae0e0edf0fe8e8ab5cdc2e56e762e0d
ioannisPApapadopoulos <john.papadopoulos0@gmail.com>
2022-03-18 12:08:34 +0000

    Add multiple element support to identity and differentiation maps

--- src/assembly.jl
+++ src/assembly.jl
@@ -2,16 +2,17 @@ using BandedMatrices: _BandedMatrix
 using ClassicalOrthogonalPolynomials: Hcat, Vcat, ∞, ℵ₀, Ones, Zeros
 
 # Identity mapping from appended sum space to dual sum space
-function idmap_append2sum(N, yu_2, yu_1, ywt0, ywt1)
-    Id = I[1:2*N+3,1:2*N+3]
-    Id = hcat(Id, yu_2)
-    Id = hcat(Id, yu_1)
-    Id = hcat(Id, ywt0)
-    Id = hcat(Id, ywt1)
+function idmap_append2sum(N, yu_2, yu_1, ywt0, ywt1; el_no=1)
+    Id = I[1:2*N+3 + (el_no-1)*(2*N+2),1:2*N+3+(el_no-1)*(2*N+2)]
+
+    Id = hcat(Id, yu_2...)
+    Id = hcat(Id, yu_1...)
+    Id = hcat(Id, ywt0...)
+    Id = hcat(Id, ywt1...)
     return Id
 end
 
-# Hilbert matrix for appended sum space
+# Hilbert matrix from sum space to sum space
 function hilbertmap(N, Tp)
     H = Matrix{Tp}(undef,2*N+3,2*N+3)
     H[:,:].= zero(Tp)
@@ -21,42 +22,51 @@ function hilbertmap(N, Tp)
     return H
 end
 
-# Differentiation matrix for appended sum space
-function diffmap(N, Tp)
+# Differentiation matrix from sum space to dual sum space
+function diffmap(N; el_no =1, Tp=Float64)
     dc1 = N+2; dc2 = N+1
-    C = Matrix{Tp}(undef, 2*N+7,2*N+3)
+    C = Matrix{Tp}(undef, 2*N+7+(el_no-1)*(2*N+6),2*N+3+(el_no-1)*(2*N+2))
     C .= zero(Tp)
-    C[3:dc1+1,2:dc1] = Diagonal(1.:dc2)
-    C[N+6:end-1,N+3:end] = Diagonal(-1.:-1:-dc2)
-    C[end, N+3:end] = zeros(1,dc2)
+    C[3:N+3,2:N+2] = Diagonal(1.:dc2)
+    C[N+6:2*N+6,N+3:2*N+3] = Diagonal(-1.:-1:-dc2)
+    # C[2*N+7, N+3:2*N+3] = zeros(1,dc2)
+    for e in 2:el_no
+        C[2*(e-1)*N+6*e-3:(2*e-1)*N+6*e-3,2*(e-1)*N+2*e:(2*e-1)*N+2*e] = Diagonal(1.:dc2)
+        C[(2*e-1)*N+6*e:2*e*N+6*e,(2*e-1)*N+2*e+1:2*e*N+2*e+1] = Diagonal(-1.:-1:-dc2)
+    end
     return C
 end
 
-# Identity map from appended sum space to dual sum space
-function idmap_sum2dual(N, Tp)
-    dc1 = N+2; dc2 = N+1
+# Identity map from sum space to dual sum space
+function idmap_sum2dual(N; el_no = 1, Tp = Float64)
+    # dc1 = N+2; dc2 = N+1
     TU = _BandedMatrix(Vcat(-Ones{Tp}(1,∞)/2,Zeros{Tp}(1,∞),Hcat(Ones{Tp}(1,1),Ones{Tp}(1,∞)/2)), ℵ₀, 0,2)
     L = _BandedMatrix(Vcat(Ones{Tp}(1,∞)/2,Zeros{Tp}(1,∞),-Ones{Tp}(1,∞)/2), ℵ₀, 2,0)
-    B = Matrix{Tp}(undef, 2*N+7,2*N+3)
+    B = Matrix{Tp}(undef, 2*N+7+(el_no-1)*(2*N+6),2*N+3+(el_no-1)*(2*N+2))
     B .= zero(Tp)
-    B[3:dc1+2,1:dc1] = TU[1:dc1,1:dc1]
+    B[3:N+4,1:N+2] = TU[1:N+2,1:N+2]
     B[2,2] = -one(Tp)/2
     B[1,1] = -one(Tp)
-    B[N+5:end,N+3:end] = L[1:N+3,1:N+1]   
+    B[N+5:2*N+7,N+3:2*N+3] = L[1:N+3,1:N+1]   
+    for e in 2:el_no
+        B[2*(e-1)*N+6*e-4:(2*e-1)*N+6*e-2,2*(e-1)*N+2*e:(2*e-1)*N+2*e] = -L[1:N+3,1:N+1]  
+        B[(2*e-1)*N+6*e-1:2*e*N+6*e+1,(2*e-1)*N+2*e+1:2*e*N+2*e+1] = L[1:N+3,1:N+1]  
+    end
     return B 
 end
 
-function idmap_append2dual(N, yu_2, yu_1, ywt0, ywt1, Tp)
-    Id = idmap_append2sum(N, yu_2, yu_1, ywt0, ywt1)
-    B = idmap_sum2dual(N, Tp)
+# Identity map from appended sum space to dual sum space
+function idmap_append2dual(N, yu_2, yu_1, ywt0, ywt1; el_no=1, Tp=Float64)
+    Id = idmap_append2sum(N, yu_2, yu_1, ywt0, ywt1, el_no=el_no)
+    B = idmap_sum2dual(N, el_no=el_no, Tp=Tp)
     return B * Id
 end
 
 
-function fractionalhelmholtzmap(λ, μ, N, Tp; a=[-1.,1.])
+function fractionalhelmholtzmap(λ, μ, N; a=[-1.,1.], el_no=1, Tp=Tp)
     H = hilbertmap(N, Tp)
     C = diffmap(N, Tp)
-    B = idmap_sum2dual(N, Tp)
+    B = idmap_sum2dual(N, el_no=el_no, Tp=Tp)
     
     scale = 1.
     el_size = a[2]-a[1]
