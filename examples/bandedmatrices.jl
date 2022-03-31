ac45e291e4ac9bd0e0d80107ac44621018915831
john papadopoulos <john.papadopoulos0@gmail.com>
2022-03-31 09:47:02 +0100

    update for banded matrices

--- examples/bandedmatrices.jl
+++ examples/bandedmatrices.jl
@@ -23,3 +23,13 @@ M = Ones(∞,∞)
 _BandedBlockBandedMatrix(M,rows,rows, (0,0), (0,0))
 
 _BandedBlockBandedMatrix(Ones(∞,∞), Fill(2,∞), Fill(2,∞), (2,0), (0,0))
+
+
+using SumSpaces
+import ClassicalOrthogonalPolynomials: ∞, mortar, Diagonal, BlockBroadcastArray
+halfvec = mortar(Fill.(1/2,Fill(2,∞)))
+d = Diagonal((-1).^(2:∞))*halfvec
+zs = mortar(Zeros.(Fill(2,∞)))
+ld = Diagonal((-1).^(1:∞))*halfvec
+dat = BlockBroadcastArray(hcat,d,zs,ld)
+A = BlockBandedMatrices._BandedBlockBandedMatrix(dat', (axes(dat,1),axes(dat,1)), (2,0), (0,0))
